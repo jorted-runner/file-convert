bd54940416b38e3e9d2ef536c2b3388406cfe746
jorted-runner <irunner1996@gmail.com>
2025-01-21 13:33:36 -0600

    starting work on final version

---
+++ final/client.py
@@ -0,0 +1,115 @@
+import socket
+import os
+from pathlib import Path
+
+def printMenu():
+    print("\n---------------------------------------\n")
+    print("1. See All Files")
+    print("2. Download File")
+    print("3. Upload File")
+    print("4. O.C.R. File")
+    print("5. O.C.R. Files in Directory")
+    print("6. Convert File to PDF")
+    print("7. Convert Files in Directory to PDF")
+    print("8. Exit")
+
+def seeAllFiles(socket):
+    files = socket.recv(1024).decode('utf-8')
+    
+    file_list = files.split('\n')
+    
+    print("Available files:")
+    for file in file_list:
+        if file.strip():
+            print(f"- {file}")
+
+def downloadFile(socket):
+    filename = input("File name: ")
+    path = input("Desired Directory Path: ")
+    if filename != 'q':
+        socket.send(filename.encode('utf-8'))
+        data = socket.recv(1024)
+        data = data.decode('utf-8')
+        if data[:6] == 'EXISTS':
+            filesize = int(data[6:])
+            message = input("File Exists. Download(Y/N) -> ")
+            if message.lower() == 'y':
+                response = "OK"
+                socket.send(response.encode('utf-8'))
+                if path != "":
+                    new_file = os.path.join(path, filename)
+                else:
+                    new_file = filename
+                with open(new_file, 'wb') as f:
+                    data = socket.recv(1024)
+                    totalReceived = len(data)
+                    f.write(data)
+                    while totalReceived < filesize:
+                        data = socket.recv(1024)
+                        totalReceived += len(data)
+                        f.write(data)
+                        print(f"Percentage Downloaded: {((totalReceived / filesize) * 100):.2f}")
+                    print("Download Complete")
+        else:
+            print("File does not exist")
+
+def uploadFile(socket):
+    filename = input("File name: ")
+    base_filename = Path(filename).name
+    if os.path.isfile(filename):
+        socket.send(base_filename.encode('utf-8'))
+        filesize = str(os.path.getsize(filename))
+        socket.send(filesize.encode('utf-8'))
+        with open(filename, 'rb') as f:
+            bytesToSend = f.read(1024)
+            while bytesToSend:
+                socket.send(bytesToSend)
+                bytesToSend = f.read(1024)
+        socket.send(b"EOF")
+    else:
+        print("File does not exist")
+
+def main():
+    host = '192.168.98.157'
+    port = 5051
+
+    try:
+        s = socket.socket()
+        s.connect((host, port))
+        print("Connected to the server.")
+
+        running = True
+        while running:
+            printMenu()
+            choice = None
+            while not choice:
+                try:
+                    choice = int(input("-> "))
+                except ValueError:
+                    print("Invalid Input, must be an integer.")
+
+            if choice == 1:
+                s.send(str(choice).encode('utf-8'))
+                seeAllFiles(s)
+            elif choice == 2:
+                s.send(str(choice).encode('utf-8'))
+                downloadFile(s)
+            elif choice == 3:
+                s.send(str(choice).encode('utf-8'))
+                uploadFile(s)
+            elif choice == 8:
+                print("Exiting the program.")
+                running = False
+            else:
+                print("Invalid choice. Please select a valid option.")
+
+        s.close()
+        print("Disconnected from the server.")
+
+    except ConnectionError as e:
+        print(f"Connection error: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
---
+++ final/server.py
@@ -0,0 +1,87 @@
+import socket
+import threading
+import os
+
+def SendFile(name, socket):
+    filename = f"server_files/{socket.recv(1024).decode('utf-8')}"
+    if os.path.isfile(filename):
+        response = "EXISTS " + str(os.path.getsize(filename))
+        socket.send(response.encode('utf-8'))
+        userResponse = socket.recv(1024)
+        userResponse = userResponse.decode("utf-8")
+        if userResponse[:2] == 'OK':
+            with open(filename, 'rb') as f:
+                bytesToSend = f.read(1024)
+                socket.send(bytesToSend)
+                while bytesToSend != "":
+                    bytesToSend = f.read(1024)
+                    socket.send(bytesToSend)
+    else:
+        response = "ERR"
+        socket.send(response.encode("utf-8"))
+
+def ReceiveFile(name, socket):
+    filename = socket.recv(1024).decode('utf-8')
+    filesize = socket.recv(1024).decode('utf-8')
+    filename = "server_files/" + filename
+    with open(filename, 'wb') as f:
+        totalReceived = 0
+        while totalReceived < int(filesize):
+            data = socket.recv(1024)
+            if data == b"EOF":
+                break
+            totalReceived += len(data)
+            f.write(data)
+
+def sendListFiles(name, socket):
+    # Walk through the directory and list files
+    directory_path = "server_files"
+    files = []
+    for root, dirs, filenames in os.walk(directory_path):
+        for filename in filenames:
+            files.append(filename)
+            
+    file_list = "\n".join(files)
+    socket.send(file_list.encode('utf-8'))
+
+def ManageConnection(name, c):
+    while True:
+        try:
+            choice = c.recv(1024).decode('utf-8')
+            if not choice:
+                break
+            elif choice == "1":
+                sendListFiles("allFilesThread", c)
+            elif choice == "2":
+                SendFile("sendThread", c)
+            elif choice == "3":
+                ReceiveFile("receiveThread", c)
+        except Exception as e:
+            print(f"Error handling client: {e}")
+            break
+    c.close()
+
+def main():
+    host = '192.168.98.157'
+    port = 5051
+
+    s = socket.socket()
+    s.bind((host, port))
+
+    s.listen(5)
+
+    print("Server Started")
+
+    while True:
+        c, addr = s.accept()
+        print("client connected ip: " + str(addr))
+
+        t = threading.Thread(target=ManageConnection, args=("manageThread", c))
+        t.start()
+
+if __name__ == "__main__":
+    main()
+
+
+
+    
